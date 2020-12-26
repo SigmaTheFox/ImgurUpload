d121cb7021c086c1ef72b943f64d275e72be7ce7
SigmaTheFox <28538408+SigmaTheFox@users.noreply.github.com>
2020-12-26 11:38:58 +0100

    added album creation/deletion and error handling

--- index.js
+++ index.js
@@ -5,8 +5,42 @@ const APIurl = "https://api.imgur.com/3";
 
 const { clientID, imageURL, albumHash, deleteHash } = require("./info.json");
 
+function createAlbum(albumTitle = "", albumDescription = "", albumPrivacy = "hidden") {
+    params.append("title", albumTitle);
+    params.append("description", albumDescription);
+    params.append("privacy", albumPrivacy);
+
+    fetch(`${APIurl}/album`, {
+        method: "post",
+        body: params,
+        headers: { "Authorization": `Client-ID ${clientID}` }
+    })
+        .then(res => res.json())
+        .catch(err => console.error(Error("There was an error")))
+        .then(json => {
+            if (json.status !== 200) return console.error(Error(json.data.error));
+            console.log(json)
+        })
+}
+
+function deleteAlbum(albumHash) {
+    if (!albumHash) return console.error(Errro("The Album Hash isn't defined"));
+
+    fetch(`${APIurl}/album/${albumHash}`, {
+        method: "delete",
+        headers: { "Authorization": `Client-ID ${clientID}` },
+        body: ""
+    })
+        .then(res => res.json())
+        .catch(err => console.error(Error("There was an error")))
+        .then(json => {
+            if (json.status !== 200) return console.error(Error(json.data.error));
+            console.log(json)
+        })
+}
+
 function uploadImage(imageURL, title = "", albumHash) {
-    if (!imageURL) return console.error(Error("No Image URL defined"));
+    if (!imageURL) return console.error(Error("The Image URL isn't defined"));
     if (albumHash) params.append("album", albumHash);
 
     params.append("image", imageURL);
@@ -19,9 +53,10 @@ function uploadImage(imageURL, title = "", albumHash) {
         headers: { "Authorization": `Client-ID ${clientID}` }
     })
         .then(res => res.json())
+        .catch(err => console.error(Error("There was an error")))
         .then(json => {
+            if (json.status !== 200) return console.error(Error(json.data.error));
             console.log(json)
-            //console.log(`Delete Hash: ${json.data.deletehash}, Image URL: ${json.data.link}`)
         })
 }
 
@@ -34,8 +69,14 @@ function deleteImage(deleteHash) {
         body: ""
     })
         .then(res => res.json())
-        .then(json => console.log(json))
+        .catch(err => console.error(Error("There was an error")))
+        .then(json => {
+            if (json.status !== 200) return console.error(Error(json.data.error));
+            console.log(json)
+        })
 }
 
-//uploadImage(imageURL)
-deleteImage(deleteHash)
+//createAlbum()
+//deleteAlbum(albumHash)
+//uploadImage(imageURL, "", albumHash)
+//deleteImage(deleteHash)
